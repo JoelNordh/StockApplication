738ae73d4859427840ebd0baf95213ca27305f9e
JoelNordh <joel.nordh@gmail.com>
2015-11-04 14:40:47 +0100

    Added Stockhandler project NOT COMPILING

---
+++ StockHandler/StockHandler.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataConverter;
+using StockHandler;
+
+namespace StockHandler
+{
+    public class StockClass
+    {
+        public DateTime date;
+        public double value;
+
+        public StockClass()
+        {
+        }
+
+        public StockClass(double value, DateTime date)
+        {
+            this.value = value;
+            this.date = date;
+        }
+    }
+
+    public class StockHandler
+    {
+        TestClass testClass = new TestClass(new CsvStockParser());
+
+        ObservableCollection<DataClass> priceList;
+        ObservableCollection<StockClass> movingAvrage20;
+        ObservableCollection<StockClass> UpperBolinger = new ObservableCollection<StockClass>();
+        ObservableCollection<StockClass> LowerBolinger = new ObservableCollection<StockClass>();
+
+        public StockHandler()
+        {
+            testClass.StockDataAdded += GotNewStockData;
+        }
+
+        private void GotNewStockData(object sender, NewDataEventArgs args)
+        {
+            priceList.Add(args.Data);
+            Console.WriteLine(args.Data.closingPrice.ToString());
+        }
+
+        private ObservableCollection<StockClass> CalculateMovingAvrage(int avrage)
+        {
+            ObservableCollection<StockClass> stockData = new ObservableCollection<StockClass>();
+            double lastAvrage = 0;
+            for (int i = avrage; i < priceList.Count; i++)
+            {
+                lastAvrage = 0;
+                for (int y = i - avrage; y < i; y++)
+                {
+                    lastAvrage += priceList[y].closingPrice;
+                }
+                lastAvrage = lastAvrage / avrage;
+
+                stockData.Add(new StockClass(lastAvrage, priceList[i].date));
+            }
+            return stockData;
+        }
+
+        private void CalculateBolinger()
+        {
+            for (int i = 0; i < movingAvrage20.Count; i++)
+            {
+                double deviationSquare = 0;
+                for (int y = 20 + i; y > i; y--)
+                {
+                    deviationSquare += Math.Pow(movingAvrage20[i].value - priceList[y].closingPrice, 2);
+                }
+                deviationSquare = Math.Sqrt(deviationSquare / 20);
+
+                UpperBolinger.Add(new StockClass(movingAvrage20[i].value + (deviationSquare * 2), movingAvrage20[i].date));
+                LowerBolinger.Add(new StockClass(movingAvrage20[i].value - (deviationSquare * 2), movingAvrage20[i].date));
+            }
+        }
+
+        public ObservableCollection<DataClass> getPriceList()
+        {
+            return priceList;
+        }
+    }
+}
---
+++ StockHandler/TestClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DataConverter;
+
+namespace StockHandler
+{
+    public class NewDataEventArgs : EventArgs
+    {
+        public DataClass Data { get; }
+
+        public NewDataEventArgs(DataClass data)
+        {
+            Data = data;
+        }
+    }
+
+    public delegate void NewDataDelegate(object sender, NewDataEventArgs args);
+    
+    public class TestClass
+    {
+        List<DataClass> testList;
+        int i;
+        public NewDataDelegate StockDataAdded;
+
+        
+
+        protected virtual void OnStockDataAdded(NewDataEventArgs args)
+        {
+            if(StockDataAdded != null)
+            {
+                StockDataAdded(this, args);
+            }
+        }
+
+        public TestClass(StockParser parser)
+        {
+            testList = parser.Parse(new System.IO.StreamReader("history.csv"));
+            testList.Reverse(); 
+            i = 0;
+        }
+
+        public void nextData()
+        {
+            OnStockDataAdded(new NewDataEventArgs(testList[i++]));
+        }
+
+        
+    }
+}
